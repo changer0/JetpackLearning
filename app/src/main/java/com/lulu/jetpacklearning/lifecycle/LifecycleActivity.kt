3931d7a91d27b5c7b81e4e1c574e76f2c66764d2
p_zlulzhang <zhanglulu@yuewen.com>
2020-08-26 15:12:17 +0800

    lifecycle 基本使用

--- app/src/main/java/com/lulu/jetpacklearning/lifecycle/LifecycleActivity.kt
+++ app/src/main/java/com/lulu/jetpacklearning/lifecycle/LifecycleActivity.kt
@@ -3,11 +3,12 @@ package com.lulu.jetpacklearning.lifecycle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lulu.jetpacklearning.R
+import kotlinx.android.synthetic.main.activity_life_cycle.*
 
 class LifecycleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle)
-        lifecycle.addObserver(LifecycleListener())
+        lifecycle.addObserver(LifecycleTestClazz(tvContent))
     }
 }
--- app/src/main/java/com/lulu/jetpacklearning/lifecycle/LifecycleTestClazz.kt
+++ app/src/main/java/com/lulu/jetpacklearning/lifecycle/LifecycleTestClazz.kt
@@ -1,6 +1,7 @@
 package com.lulu.jetpacklearning.lifecycle
 
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
@@ -9,21 +10,31 @@ import androidx.lifecycle.OnLifecycleEvent
  * @author zhanglulu on 2020/8/26.
  * for 一个 lifecycle 的观察者
  */
-private const val TAG = "LifecycleListener"
-class LifecycleListener: LifecycleObserver {
-
+private const val TAG = "LifecycleTestClazz"
+class LifecycleTestClazz(val tvContent: TextView): LifecycleObserver {
+    val loggerStr = StringBuilder()
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onActivityCreate() {
         Log.d(TAG, "onActivityCreate: ")
+        loggerStr.append("onActivityCreate\n")
+        refresh()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onActivityResume() {
         Log.d(TAG, "onActivityResume: ")
+        loggerStr.append("onActivityResume\n")
+        refresh()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onActivityPause() {
         Log.d(TAG, "onActivityPause: ")
+        loggerStr.append("onActivityPause\n")
+        refresh()
+    }
+
+    private fun refresh() {
+        tvContent.text = loggerStr.toString()
     }
 }
